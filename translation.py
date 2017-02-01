895da2700d97a3e1da7d41f684db6aca33d515ed
Stanislav Baltrunas <stanislav@baltrunas.ru>
2017-02-01 20:51:40 +0600

    add translations

--- translation.py
+++ translation.py
@@ -1,10 +1,28 @@
 from modeltranslation.translator import translator
 from modeltranslation.translator import TranslationOptions
 
-from .models import Office
+from .models import Office, OfficeFeature, FormConfig, FormField
 
 
 class OfficeTranslationOptions(TranslationOptions):
-	fields = ['name', 'description', 'orgdata', 'address', 'www']
+	fields = ['name', 'description', 'address_country', 'address_locality', 'address_region', 'address_box', 'address_postal', 'address_street']
 
 translator.register(Office, OfficeTranslationOptions)
+
+
+class OfficeFeatureTranslationOptions(TranslationOptions):
+	fields = ['name', 'value']
+
+translator.register(OfficeFeature, OfficeFeatureTranslationOptions)
+
+
+class FormConfigTranslationOptions(TranslationOptions):
+	fields = ['title', 'submit_name', 'success_message']
+
+translator.register(FormConfig, FormConfigTranslationOptions)
+
+
+class FormFieldTranslationOptions(TranslationOptions):
+	fields = ['label', 'choices']
+
+translator.register(FormField, FormFieldTranslationOptions)
